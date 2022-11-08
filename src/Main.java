598fb05c0f61be96700af4992cddae4653da619e
MrGibbs90 <mrgibbs9090@mail.ru>
2022-11-08 23:08:57 +0700

    Практическая часть курсовой

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String characterSet = "Я родился в 1670 году";
+        String[] symbols = characterSet.split(" ");
+        for (String symbol : symbols) {
+            System.out.println(symbol);
+        }
     }
 }
